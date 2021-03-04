9201c5164eda6bfaedf0bea14a103e69616c0926
lateralusd <xdaemonx@protonmail.ch>
2021-03-05 00:16:13 +0100

    A bit of refactoring

---
+++ handler/handler.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/lateralusd/shorty/db"
+)
+
+type Env struct {
+	DB *db.Config
+}
+
+type Handler struct {
+	*Env
+	H func(e *Env, w http.ResponseWriter, r *http.Request)
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.H(h.Env, w, r)
+}
+
+func IndexPath(env *Env, w http.ResponseWriter, r *http.Request) {
+	shorted := strings.TrimLeft(r.URL.Path, "/")
+	url, err := env.DB.GetShorted(shorted)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "URL is not shorted")
+	} else {
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	}
+}
+
+func ShortyPath(env *Env, w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintln(w, "Error fetching form data")
+		return
+	}
+	url := r.FormValue("url")
+	shorted, err := env.DB.SaveLink(url)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Something went wrong")
+	}
+
+	/* TODO: Do not hardcode link */
+	link := "https://localhost:8080/" + shorted
+	fmt.Fprintln(w, link)
+}
--- main.go
+++ main.go
@@ -4,19 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/lateralusd/shorty/db"
+	"github.com/lateralusd/shorty/handler"
 )
-
-type Env struct {
-	DB *db.Config
-}
-
-type Handler struct {
-	*Env
-	H func(e *Env, w http.ResponseWriter, r *http.Request)
-}
 
 func main() {
 	var err error
@@ -25,45 +16,13 @@ func main() {
 		log.Fatal("Coult not initialize database", err)
 	}
 
-	env := &Env{
+	env := &handler.Env{
 		DB: database,
 	}
 
-	http.Handle("/", Handler{env, IndexPath})
-	http.Handle("/shorty", Handler{env, ShortyPath})
+	http.Handle("/", handler.Handler{env, handler.IndexPath})
+	http.Handle("/shorty", handler.Handler{env, handler.ShortyPath})
 
 	fmt.Println("[*] Starting the server")
 	http.ListenAndServe(":8080", nil)
-}
-
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.H(h.Env, w, r)
-}
-
-func IndexPath(env *Env, w http.ResponseWriter, r *http.Request) {
-	shorted := strings.TrimLeft(r.URL.Path, "/")
-	url, err := env.DB.GetShorted(shorted)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, "URL is not shorted")
-	} else {
-		http.Redirect(w, r, url, http.StatusSeeOther)
-	}
-}
-
-func ShortyPath(env *Env, w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintln(w, "Error fetching form data")
-		return
-	}
-	url := r.FormValue("url")
-	shorted, err := env.DB.SaveLink(url)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, "Something went wrong")
-	}
-
-	/* TODO: Do not hardcode link */
-	link := "https://localhost:8080/" + shorted
-	fmt.Fprintln(w, link)
 }
